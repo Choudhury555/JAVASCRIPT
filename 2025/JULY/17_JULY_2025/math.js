ab32813c209409b107a8a13b684b2af3f8d8d83d
Choudhury555 <choudhurypanda143@gmail.com>
2025-07-17 19:00:36 +0530

    17 July Practice

---
+++ 2025/JULY/17_JULY_2025/math.js
@@ -0,0 +1,4 @@
+//Named Exports
+
+export const add = (a,b) => a+b;
+export const multiply = (a,b) => a*b;
---
+++ 2025/JULY/17_JULY_2025/p1.js
@@ -0,0 +1,13 @@
+// JavaScript Modules
+
+
+// Why Modules?
+
+// - Organize code into separate files
+// - Avoid global variable pollution
+// - Reuse functions, classes, constants across files
+
+import { add, multiply } from "./math.js";
+
+console.log(add(3,4));
+console.log(multiply(7,3));
