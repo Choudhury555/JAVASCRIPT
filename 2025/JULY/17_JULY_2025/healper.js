12218be194327797eb0105749ebf220ac26392e6
Choudhury555 <choudhurypanda143@gmail.com>
2025-07-19 09:25:39 +0530

    19 July Practice

---
+++ 2025/JULY/17_JULY_2025/healper.js
@@ -0,0 +1,7 @@
+// Mixing Named & Default Exports
+
+export default function log(msg){
+    console.log(msg);
+}
+
+export const PI = 3.14159;
--- 2025/JULY/17_JULY_2025/p1.js
+++ 2025/JULY/17_JULY_2025/p1.js
@@ -7,6 +7,7 @@
 // - Avoid global variable pollution
 // - Reuse functions, classes, constants across files
 
+import log, { PI } from "./healper.js";
 import { add, multiply } from "./math.js";//Named Exports
 import greet from "./utils.js";//Default Export
 
@@ -14,3 +15,6 @@ console.log(add(3,4));
 console.log(multiply(7,3));
 
 console.log(greet("Abhisek"));
+
+log("This is from helper.js file");
+console.log(PI);
